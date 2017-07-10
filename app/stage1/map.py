33ca0385ad496881261121b796e320963ca1cf89
estwanick <estwanick@gmail.com>
2017-07-09 20:25:47 -0400

    Output only ticker, date, closing price

--- app/stage1/map.py
+++ app/stage1/map.py
@@ -7,6 +7,8 @@ FILENAME = re.search('[^\/]+(\w+)$', PATH).group().replace('.csv', '')
 idx = 0
 for line in sys.stdin:
     line = line.strip()
+    row = line.split(',')
+    #Skip header row
     if idx != 0:
-        print FILENAME + "," + line
+        print FILENAME + "," + row[0] + "," + row[4]
     idx = idx + 1
