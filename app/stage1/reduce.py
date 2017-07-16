8fe6f35e8c033323c19ae8895ecb99d2b0569885
Matt <matthewjlee2@gmail.com>
2017-07-16 17:53:51 -0400

    Adding other models

--- app/stage1/reduce.py
+++ app/stage1/reduce.py
@@ -2,15 +2,19 @@
 
 import sys
 import numpy as np
-
+from sklearn.svm import SVR
 ticker = None
 closingPrice = None
 month = None
 year = None
 stdDeviation = 0
+linearVar = 0
+polyVar = 0
+rbfVar = 0
+
 #Hold daily prices for that month
 dailyClosingPrices = []
-
+dates = []
 for line in sys.stdin:
     line = line.strip()
     lineParams = line.split(',')
@@ -21,14 +25,36 @@ for line in sys.stdin:
     cOpeningPrice = float(lineParams[4])
     cClosingPrice = float(lineParams[5])
 
+    #initialize the svr objects for each model
+    svrlin = SVR(kernel = 'linear', C=1e3)
+    svrpoly = SVR(kernel = 'poly', C=1e3, degree = 2)
+    svrrbf = SVR(kernel = 'rbf', C=1e3, gamma = 0.1)
+    
     if (ticker == cTicker) and (month == cMonth) and (year == cYear):
         dailyClosingPrices.append(cClosingPrice)
+        dates.append(cYear)
     else:
         if ticker:
             #print(dailyClosingPrices)
-            stdDeviation = np.std(dailyClosingPrices)
-            print '%s,%s,%s,%s' % (ticker, year, month, stdDeviation)
+            dates = np.reshape(dates, len(dates), 1)
+            #fit the data for each model
+            svrlin.fit(dates, dailyClosingPrices)
+            svrpoly.fit(dates, dailyClosingPrices)
+            svrrbf.fit(dates, dailyClosingPrices)
+            #calculate variance for each model
+            linearVar = svrlin.predict(dailyClosingPrices)
+            polyVar = svrpoly.predict(dailyClosingPrices)
+            rbfVar = svrrbf.predict(dailyClosingPrices)
+            #create output strings for each
+            linearOutput = 'linear: ' + linearVar + '; '
+            polyOutput = 'poly: ' + polyVar + '; '
+            rbfOutput = 'rbf: ' + '; '
+
+            #stdDeviation = np.std(dailyClosingPrices)
+
+            print '%s,%s,%s,%s,%s,%s' % (ticker, year, month, linearOutput, polyOutput, rbfOutput)
             dailyClosingPrices = []
+            dates = []
 
         ticker = cTicker
         year = cYear
@@ -38,5 +64,19 @@ for line in sys.stdin:
 # Just ignore last month for now
 if (ticker == cTicker) and (month == cMonth) and (year == cYear):
     #print(dailyClosingPrices)
-    stdDeviation = np.std(dailyClosingPrices)
-    print '%s,%s,%s,%s' % (ticker, year, month, stdDeviation)
+    #stdDeviation = np.std(dailyClosingPrices)
+    
+    #fit the data for each model
+    svrlin.fit(dates, dailyClosingPrices)
+    svrpoly.fit(dates, dailyClosingPrices)
+    svrrbf.fit(dates, dailyClosingPrices)
+    #calculate variance for each model
+    linearVar = svrlin.predict(dailyClosingPrices)
+    polyVar = svrpoly.predict(dailyClosingPrices)
+    rbfVar = svrrbf.predict(dailyClosingPrices)
+    #create output strings for each
+    linearOutput = 'linear: ' + linearVar + '; '
+    polyOutput = 'poly: ' + polyVar + '; '
+    rbfOutput = 'rbf: ' + '; '
+    
+    print '%s,%s,%s,%s,%s,%s' % (ticker, year, month, linearOutput, polyOutput, rbfOutput)
