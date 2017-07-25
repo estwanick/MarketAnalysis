2d7c7e6d392a589353e75b3f8c7b7c0c344103c8
Matt <matthewjlee2@gmail.com>
2017-07-24 22:46:44 -0400

    meeting 7.24 code

--- app/stage2/reduce.py
+++ app/stage2/reduce.py
@@ -4,6 +4,8 @@ import sys
 import numpy as np
 from sklearn.svm import SVR
 from sklearn import linear_model
+import matplotlib.pyplot as plt
+
 ticker = None
 closingPrice = None
 month = None
@@ -26,38 +28,40 @@ for line in sys.stdin:
     monthlyReturn = lineParams[3]
     #initialize the svr objects for each model
     svrlin = SVR(kernel = 'linear', C=1e3)
-    svrrbf = SVR(kernel = 'rbf', C=1000, gamma=10)
-    svrpoly = SVR(kernel = 'poly', C=1000, gamma=10, max_iter=15, degree=3)
+    svrrbf = SVR(kernel = 'rbf', C=1e3, gamma=10)
+    svrpoly = SVR(kernel = 'poly', C=1e3, gamma=10, max_iter=15, degree=2)
     #lm = linear_model.LinearRegression()
 
-    if (ticker == cTicker) and (year == cYear):
+    if (ticker == cTicker) and (month == cMonth):
         monthlyReturns.append(monthlyReturn)
         dates.append(cYear)
         print '%s,%s,%s,%s' % (cTicker, cYear, cMonth, monthlyReturn)
     else:
         if ticker:
-            dataX = np.array(dates).astype(np.float).reshape((len(dates),-1))
+            dataX = np.array(dates).reshape((len(dates),-1))
             dataY = np.array(monthlyReturns).astype(np.float)
             
             #lm.fit(dataX, dataY)
             #orgLm = lm.predict(2016)
             #orgLmOutput = ('%s,%s,%s') % ('Original linear: ', orgLm, '; ')
-
+            
             #fit the data for each model
             svrlin.fit(dataX, dataY)
             svrpoly.fit(dataX, dataY)
             svrrbf.fit(dataX, dataY)
             #calculate variance for each model
-            linearVar = svrlin.predict(cYear)
-            polyVar = svrpoly.predict(cYear)
-            rbfVar = svrrbf.predict(cYear)
+            linearVar = svrlin.predict(2016)
+            polyVar = svrpoly.predict(6)
+            rbfVar = svrrbf.predict(2016)
             
             linearOutput = '%s%s%s' % ('linear: ', linearVar, '; ')
             polyOutput = '%s%s%s' % ('poly: ', polyVar, '; ')
             rbfOutput = '%s%s%s' % ('rbf: ', rbfVar, '; ')
             #harcode 2016 because that is what we are predicting
 
-            print '%s,%s,%s,%s,%s' % (ticker, '-------Prediction for january ' + cYear, linearOutput, polyOutput, rbfOutput)
+          
+
+            print '%s,%s,%s,%s,%s' % (ticker, '-------Prediction for ' + cMonth + ' 2017', linearOutput, polyOutput, rbfOutput)
             print '%s,%s,%s,%s' % (cTicker, cYear, cMonth, monthlyReturn)
 
             monthlyReturns = []
@@ -70,8 +74,8 @@ for line in sys.stdin:
         dates.append(cYear)
 
 # Just ignore last month for now
-if (ticker == cTicker) and (year == cYear):
-    dataX = np.array(dates).reshape((len(dates),-1))
+if (ticker == cTicker) and (month == cMonth):
+    dataX = np.array(dates).reshape(len(dates),-1)
     dataY = np.array(monthlyReturns)
             
     #fit the data for each model
@@ -79,9 +83,9 @@ if (ticker == cTicker) and (year == cYear):
     svrpoly.fit(dataX, dataY)
     svrrbf.fit(dataX, dataY)
     
-    linearVar = svrlin.predict(2017-8)
+    linearVar = svrlin.predict(2016)
     polyVar = svrpoly.predict(2017-8)
-    rbfVar = svrrbf.predict(2017-8)
+    rbfVar = svrrbf.predict(2016)
     
     linearOutput = '%s%s%s' % ('linear: ', linearVar, '; ')
     rbfOutput = '%s%s%s' % ('rbf: ', rbfVar, '; ')
